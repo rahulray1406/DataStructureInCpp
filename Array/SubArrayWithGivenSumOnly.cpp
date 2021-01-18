780dba08ab0d64232d99508b2fc896eaad9f3e9c
rahuray1406 <rahulray1532@gmail.com>
2021-01-18 23:04:47 +0530

    modification in subarray with given sum

--- Array/SubArrayWithGivenSumOnly.cpp
+++ Array/SubArrayWithGivenSumOnly.cpp
@@ -4,26 +4,18 @@
 using namespace std;
 int checkIfexist(int arr[],int len,int sum)
 {
-    int curr_sum=arr[0],start=0,i;
-    for(i=1;i<len;i++)
-    {
-        while (curr_sum>sum && start<i-1)
+    int curr_sum=0,start=0;
+    for(int i=0;i<len;i++)
+    {   curr_sum+=arr[i];
+        while (curr_sum>sum)
         {
             curr_sum-=arr[start];
-            start++;
-           
+            start++;  
         }
         if (curr_sum==sum) 
-        {    cout<<start<<" "<<i-1;
+        {    cout<<start+1<<" "<<i+1;
               return 1;           
-        }
-        
-        if (i<len)
-        {    
-            curr_sum+=arr[i];
-           
-        }  
-            
+        }   
     }
     
     return -1;
