3f55b337fc17cd50239bcd5934f68bf22258204e
rahuray1406 <rahulray1532@gmail.com>
2021-02-11 02:22:36 +0530

    setting

---
+++ Hashing/CountDistinctEle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+int main()
+{
+    int len;
+    cout<<"Enter the len of array\n";
+    cin>>len;
+    int arr[len];
+    cout<<"Enter the array element\n";
+    for(int i=0;i<len;i++)
+    {
+        cin>>arr[i];
+    }
+    unordered_set<int> us(arr,arr+len);
+    cout<<"Number of distinct elements are.. "<<us.size();
+    return 0;
+
+}
