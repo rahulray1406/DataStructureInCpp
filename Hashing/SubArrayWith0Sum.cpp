0dba2fcb369921701996bd7cdb199c594be4c69a
rahuray1406 <rahulray1532@gmail.com>
2021-02-27 20:55:50 +0530

    Sub Array with 0 sum showing index

--- Hashing/SubArrayWith0Sum.cpp
+++ Hashing/SubArrayWith0Sum.cpp
@@ -1,38 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-bool subArrayZeroSum(int arr[],int len)
-{   unordered_set<int> set;
-    int prefixSum=0;
-    for(int i=0;i<len;i++)
-    {   prefixSum+=arr[i];
-        if(set.find(prefixSum)!=set.end())
+bool subArrayZeroSum(int arr[], int len)
+{
+    unordered_map<int, int> map;
+    int prefixSum = 0;
+    for (int i = 0; i < len; i++)
+    {
+        prefixSum += arr[i];
+        if (map.find(prefixSum) != map.end())
+        {
+            cout << "index " << map[prefixSum] + 1 << " " << i << endl;
             return true;
-        if(prefixSum==0)
+        }
+        if (prefixSum == 0)
+        {
+            cout << "index " << 0 << " " << i << endl;
             return true;
-        set.insert(prefixSum);
+        }
+        map[prefixSum] = i;
     }
     return false;
-
 }
 int main()
 {
     int len;
-    cout<<"Enter the len of array\n";
-    cin>>len;
+    cout << "Enter the len of array\n";
+    cin >> len;
     int arr[len];
-    cout<<"Enter the array ellements\n";
-    for(int i=0;i<len;i++)
+    cout << "Enter the array ellements\n";
+    for (int i = 0; i < len; i++)
     {
-       
-        cin>>arr[i];
+
+        cin >> arr[i];
     }
-    if(subArrayZeroSum(arr,len))
+    if (subArrayZeroSum(arr, len))
     {
-        cout<<"YES"<<endl;
+        cout << "YES" << endl;
     }
     else
     {
-        cout<<"NO"<<endl;
+        cout << "NO" << endl;
     }
-    
 }
