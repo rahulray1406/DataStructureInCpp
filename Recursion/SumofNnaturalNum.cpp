d8f37d134fc520c6184c9d647531ffe14d3a1973
rahuray1406 <rahulray1532@gmail.com>
2021-04-22 21:55:27 +0530

    sum of n natural numbers

---
+++ Recursion/SumofNnaturalNum.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int n)
+{   
+    if(n==0)return 0;
+     return n+sum(n-1);
+}
+int main()
+{
+    int N;
+    cin>>N;
+    cout<<sum(N);
+    return 0;
+}
